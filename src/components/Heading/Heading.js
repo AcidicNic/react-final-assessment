edd621a2e0a6dcb32f9c7abdcd2b2e227f183b86
Nicole Abadie <nic4096@gmail.com>
2021-12-07 16:44:38 -0800

    setup project

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,15 @@
+import './Heading.css';
+
+export default function Heading(props) {
+  const totalBands = 0; 
+  const totalStyles = 0;
+
+  return (
+    <div className="heading">
+      <h1>Nicole Rocha Abadie</h1>
+      <p>FEW 1.2 Final Assessment</p>
+      <p>Total Bands: { totalBands }</p>
+      <p>Styles: { totalStyles }</p>
+    </div>
+  )
+}
